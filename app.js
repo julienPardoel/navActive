bc8c60856d9db6a11afecf50df5c6d99870515f1
julien <julien.pardoel@gmail.com>
2022-09-24 23:10:53 +0200

    js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("ok");
+const tabs = [...document.querySelectorAll(".tab")];
+
+const tabsAnimation = (e) => {
+  const tabContents = [...document.querySelectorAll(".tab-content")];
+
+  const indexToRemove = tabs.findIndex((tab) =>
+    tab.classList.contains("active-tab")
+  );
+
+  tabs[indexToRemove].classList.remove("active-tab");
+  tabContents[indexToRemove].classList.remove("active-tab-content");
+
+  const indexToShow = tabs.indexOf(e.target);
+
+  tabs[indexToShow].classList.add("active-tab");
+  tabContents[indexToShow].classList.add("active-tab-content");
+};
+
+tabs.forEach((tab) => tab.addEventListener("click", tabsAnimation));
